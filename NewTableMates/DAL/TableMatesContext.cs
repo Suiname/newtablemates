5409b72466c6c9217e17695b830313f69d520988
suiname <jtham@sbcglobal.net>
2016-02-01 14:24:32 -0600

    added DB context

---
+++ NewTableMates/DAL/TableMatesContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using NewTableMates.Models;
+
+namespace NewTableMates.DAL
+{
+    public class TableMatesContext : DbContext
+    {
+        public TableMatesContext() : base("TableMatesContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}

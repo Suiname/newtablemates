cf810e9534585f4bc1e7286f98866d066500d2bf
suiname <jtham@sbcglobal.net>
2016-02-01 14:14:12 -0600

    added User model

---
+++ NewTableMates/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NewTableMates.Models
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
+        public string username { get; set; }
+        public string email { get; set; }
+        public virtual ICollection<Appointment> Appointments { get; set; }
+    }
+}

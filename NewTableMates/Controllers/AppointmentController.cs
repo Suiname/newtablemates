f5d0abbb0b1936a96851b161f920950757a19259
suiname <jtham@sbcglobal.net>
2016-02-01 14:42:51 -0600

    added appointment and user controllers

---
+++ NewTableMates/Controllers/AppointmentController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using NewTableMates.DAL;
+using NewTableMates.Models;
+
+namespace NewTableMates.Controllers
+{
+    public class AppointmentController : ApiController
+    {
+        private TableMatesContext db = new TableMatesContext();
+
+        // GET: api/Appointment
+        public IQueryable<Appointment> GetAppointments()
+        {
+            return db.Appointments;
+        }
+
+        // GET: api/Appointment/5
+        [ResponseType(typeof(Appointment))]
+        public IHttpActionResult GetAppointment(int id)
+        {
+            Appointment appointment = db.Appointments.Find(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(appointment);
+        }
+
+        // PUT: api/Appointment/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAppointment(int id, Appointment appointment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != appointment.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(appointment).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AppointmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Appointment
+        [ResponseType(typeof(Appointment))]
+        public IHttpActionResult PostAppointment(Appointment appointment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Appointments.Add(appointment);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = appointment.ID }, appointment);
+        }
+
+        // DELETE: api/Appointment/5
+        [ResponseType(typeof(Appointment))]
+        public IHttpActionResult DeleteAppointment(int id)
+        {
+            Appointment appointment = db.Appointments.Find(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            db.Appointments.Remove(appointment);
+            db.SaveChanges();
+
+            return Ok(appointment);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AppointmentExists(int id)
+        {
+            return db.Appointments.Count(e => e.ID == id) > 0;
+        }
+    }
+}
